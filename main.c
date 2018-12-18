6da90cdbb568c6c9330fc4161b03ac8acce46b20
PeterStine <peter.stine@oit.edu>
2018-12-17 19:47:33 -0600

    Added transceiver code files

---
+++ main.c
@@ -0,0 +1,27 @@
+/* Author: 			Peter Stine, <others>
+ * Date Created:		12/17/18
+ * Last Modification Date:	12/17/18
+ * Filename:			main.c
+ * Dependency Level:		Top-Level
+ *
+ * Functional Overview:
+ *
+ * 	This program will utilize several other c-language libraries similar in function
+ * 	to the github repositories listed in the references section of this header block.
+ * 	This main.c file will act as the top level dependency for the LCD driver and
+ * 	Radio Transciever Module Driver functions.
+ *
+ * Junior Project Specific Use Policies:
+ *
+ * 	For maintaining integrity of the function of this code repository, only the person
+ * 	who's name is listed at the beginning of the file is permitted to write changes to
+ * 	that file. All other persons will be allowed read-only access to a file where 
+ * 	their name does not appear in the header.
+ *
+*/
+
+int main()
+{
+	// Run functions here.
+	return 0;
+}
