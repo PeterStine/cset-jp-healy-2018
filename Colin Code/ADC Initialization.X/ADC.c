5a372dcda36d77bd97115fc51091b951e15cd637
PoauVietnam <colin.lundquist@oit.edu>
2019-01-28 17:15:12 -0800

    Finished ADC functionality. Untested.

--- Colin Code/ADC Initialization.X/ADC.c
+++ Colin Code/ADC Initialization.X/ADC.c
@@ -1,50 +1,25 @@
 #include <xc.h>
 #include <sys/attribs.h>
+#include "ADC.h"
 
-#include <stdint.h>
-
-#define u32 uint32_t
-#define u8 uint8_t
-
-// PIC32MZ2048ECG144 Configuration Bit Settings
-// DEVCFG2
-#pragma config FPLLIDIV = DIV_2         // System PLL Input Divider (2x Divider) 12MHz/2 = 6MHz
-#pragma config FPLLRNG = RANGE_5_10_MHZ // System PLL Input Range (5-10 MHz Input)
-#pragma config FPLLICLK = PLL_POSC      // System PLL Input Clock Selection (POSC is input to the System PLL)
-#pragma config FPLLMULT = MUL_112       // System PLL Multiplier (PLL Multiply by 112) 6MHz * 112 = 672MHz
-#pragma config FPLLODIV = DIV_8         // System PLL Output Clock Divider (8x Divider) 672MHz / 8 = 84MHz
-#pragma config UPLLFSEL = FREQ_12MHZ    // USB PLL Input Frequency Selection (USB PLL input is 12 MHz)
-#pragma config UPLLEN = OFF             // USB PLL Enable (USB PLL is disabled)
-
-// DEVCFG1
-#pragma config FNOSC = SPLL             // Oscillator Selection Bits (Primary Osc (HS,EC))
-#pragma config FSOSCEN = OFF            // Secondary Oscillator Enable (Disable SOSC)
-#pragma config POSCMOD = HS             // Primary Oscillator Configuration (HS osc mode)
-#pragma config FCKSM = CSDCMD           // Clock Switching and Monitor Selection (Clock Switch Disabled, FSCM Disabled)
-#pragma config FWDTEN = OFF             // Watchdog Timer Enable (WDT Disabled)
-#pragma config FDMTEN = OFF             // Deadman Timer Enable (Deadman Timer is disabled)
-
-// DEVCFG0
-#pragma config JTAGEN = OFF             // JTAG Enable (JTAG Disabled)
-#pragma config ICESEL = ICS_PGx2        // ICE/ICD Comm Channel Select (Communicate on PGEC2/PGED2)
-
-
-int main(int argc, char** argv) {
-    int result[3];
+void ADC_Init(void) {
+    
      /* Set up the CAL registers */
      AD1CAL1 = DEVADC1; // Copy the configuration data to the AD1CAL2 = DEVADC2;
-    // AD1CALx special function registers.
+     AD1CAL2 = DEVADC2;
      AD1CAL3 = DEVADC3;
      AD1CAL4 = DEVADC4;
      AD1CAL5 = DEVADC5;
+     
      /* Configure AD1CON1 */
-     AD1CON1 = 0; // No AD1CON1 features are enabled including: Stop-in-Idle, early
-    // interrupt, filter delay Fractional mode and scan trigger source.
+     AD1CON1 = 0;
 
      /* Configure AD1CON2 */
-     AD1CON2 = 0; // Boost, Low-power mode off, SAMC set to min, set up the ADC Clock
+     AD1CON2 = 0;
+     AD1CON2 = 0x4000; // Boost on
      AD1CON2bits.ADCSEL = 1; // 1 = SYSCLK, 2 REFCLK03, 3 FRC
      AD1CON2bits.ADCDIV = 4; // TQ = 1/200 MHz; Tad = 4* (TQ * 2) = 40 ns; 25 MHz ADC clock
+     
      /* Configure AD1CON3 */
      AD1CON3 = 0; // ADINSEL is not configured for this example. VREFSEL of ?0?
     // selects AVDD and AVSS as the voltage reference.
@@ -59,11 +34,12 @@ int main(int argc, char** argv) {
      AD1CSS2 = 0;
      /* Configure AD1CMPCONx */
      AD1CMPCON1 = 0; // No digital comparators are used. Setting the AD1CMPCONx
-     AD1CMPCON2 = 0; // register to ?0? ensures that the comparator is disabled. Other
-     AD1CMPCON3 = 0; // registers are ?don?t care?.
+     AD1CMPCON2 = 0; // register to 0 ensures that the comparator is disabled. Other
+     AD1CMPCON3 = 0; // registers are dont care.
      AD1CMPCON4 = 0;
      AD1CMPCON5 = 0;
      AD1CMPCON6 = 0;
+     
      /* Configure AD1FLTRx */
      AD1FLTR1 = 0; // No oversampling filters are used.
      AD1FLTR2 = 0;
@@ -71,36 +47,101 @@ int main(int argc, char** argv) {
      AD1FLTR4 = 0;
      AD1FLTR5 = 0;
      AD1FLTR6 = 0;
+     
      /* Set up the trigger sources */
      AD1TRG1 = 0; // Initialize all sources to no trigger.
      AD1TRG2 = 0;
      AD1TRG3 = 0;
+     
+     
      AD1TRG1bits.TRGSRC0 = 1; // Set AN0 to trigger from software.
      AD1TRG1bits.TRGSRC1 = 1; // Set AN1 to trigger from software.
      AD1TRG1bits.TRGSRC2 = 1; // Set AN2 to trigger from software.
+     AD1TRG1bits.TRGSRC3 = 1; // Set AN3 to trigger from software.
+     AD1TRG2bits.TRGSRC4 = 1; // Set AN4 to trigger from software.
+     AD1TRG2bits.TRGSRC5 = 1; // Set AN5 to trigger from software.
+     
      /* Turn the ADC on, start calibration */
      AD1CON1bits.ADCEN = 1;
      /* Wait for calibration to complete */
      while (AD1CON2bits.ADCRDY == 0);
-     
-    while (1) {
-    /* Trigger a conversion */
-    AD1CON3bits.GSWTRG = 1;
-    /* Wait the conversions to complete */
-    while (AD1DSTAT1bits.ARDY0 == 0);
-    /* fetch the result */
-    result[0] = AD1DATA0;
-  
-    int i = 0;
-    while (i < 90000) { i++; asm("nop"); }
-    /*
-   * Process results here
-   *
-   * Note 1: Loop time determines the sampling time since all inputs are Class 1.
-   * A delay may be needed to meet sample time requirements.
-   * Note 2: The first 5 samples may have reduced accuracy.
-   *
-   */
+
+     return;
+}
+
+uint16_t ADC_READ(uint8_t inputSel){
+    
+    uint16_t retVal = 0x0000;
+    
+    switch (inputSel){
+        
+        case 0:{
+        
+            if (AD1TRG1bits.TRGSRC0) {// If The input is under software trigger, trigger it. 
+                /* Trigger a conversion */
+                AD1CON3bits.GSWTRG = 1;
+                /* Wait the conversions to complete */
+                while (AD1DSTAT1bits.ARDY0 == 0);
+                
+            }
+            retVal = AD1DATA0; // get data from encoder
+        }
+        case 1:{
+        
+            if (AD1TRG1bits.TRGSRC1) {// If The input is under software trigger, trigger it. 
+                /* Trigger a conversion */
+                AD1CON3bits.GSWTRG = 1;
+                /* Wait the conversions to complete */
+                while (AD1DSTAT1bits.ARDY1 == 0);
+                
+            }
+            retVal = AD1DATA1; // get data from encoder
+        }
+        case 2:{
+        
+            if (AD1TRG1bits.TRGSRC2) {// If The input is under software trigger, trigger it. 
+                /* Trigger a conversion */
+                AD1CON3bits.GSWTRG = 1;
+                /* Wait the conversions to complete */
+                while (AD1DSTAT1bits.ARDY2 == 0);
+                
+            }
+            retVal = AD1DATA2; // get data from encoder
+        }
+        case 3:{
+        
+            if (AD1TRG1bits.TRGSRC3) {// If The input is under software trigger, trigger it. 
+                /* Trigger a conversion */
+                AD1CON3bits.GSWTRG = 1;
+                /* Wait the conversions to complete */
+                while (AD1DSTAT1bits.ARDY3 == 0);
+                
+            }
+            retVal = AD1DATA3; // get data from encoder
+        }
+        case 4:{
+        
+            if (AD1TRG2bits.TRGSRC4) {// If The input is under software trigger, trigger it. 
+                /* Trigger a conversion */
+                AD1CON3bits.GSWTRG = 1;
+                /* Wait the conversions to complete */
+                while (AD1DSTAT1bits.ARDY4 == 0);
+                
+            }
+            retVal = AD1DATA4; // get data from encoder
+        }
+        case 5:{
+        
+            if (AD1TRG2bits.TRGSRC5) {// If The input is under software trigger, trigger it. 
+                /* Trigger a conversion */
+                AD1CON3bits.GSWTRG = 1;
+                /* Wait the conversions to complete */
+                while (AD1DSTAT1bits.ARDY5 == 0);
+                
+            }
+            retVal = AD1DATA5; // get data from encoder
+        }
     }
-   return (1);
-   }
+    
+    return(retVal);
+}
--- Colin Code/ADC Initialization.X/nbproject/Makefile-default.mk
+++ Colin Code/ADC Initialization.X/nbproject/Makefile-default.mk
@@ -57,17 +57,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=ADC_Init.c
+SOURCEFILES_QUOTED_IF_SPACED=ADC.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/ADC_Init.o
-POSSIBLE_DEPFILES=${OBJECTDIR}/ADC_Init.o.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/ADC.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/ADC.o.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/ADC_Init.o
+OBJECTFILES=${OBJECTDIR}/ADC.o
 
 # Source Files
-SOURCEFILES=ADC_Init.c
+SOURCEFILES=ADC.c
 
 
 CFLAGS=
@@ -106,18 +106,18 @@ endif
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
-${OBJECTDIR}/ADC_Init.o: ADC_Init.c  nbproject/Makefile-${CND_CONF}.mk
+${OBJECTDIR}/ADC.o: ADC.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
-	@${RM} ${OBJECTDIR}/ADC_Init.o.d 
-	@${RM} ${OBJECTDIR}/ADC_Init.o 
-	@${FIXDEPS} "${OBJECTDIR}/ADC_Init.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_REAL_ICE=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/ADC_Init.o.d" -o ${OBJECTDIR}/ADC_Init.o ADC_Init.c    -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD) 
+	@${RM} ${OBJECTDIR}/ADC.o.d 
+	@${RM} ${OBJECTDIR}/ADC.o 
+	@${FIXDEPS} "${OBJECTDIR}/ADC.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/ADC.o.d" -o ${OBJECTDIR}/ADC.o ADC.c    -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD) 
 	
 else
-${OBJECTDIR}/ADC_Init.o: ADC_Init.c  nbproject/Makefile-${CND_CONF}.mk
+${OBJECTDIR}/ADC.o: ADC.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} "${OBJECTDIR}" 
-	@${RM} ${OBJECTDIR}/ADC_Init.o.d 
-	@${RM} ${OBJECTDIR}/ADC_Init.o 
-	@${FIXDEPS} "${OBJECTDIR}/ADC_Init.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/ADC_Init.o.d" -o ${OBJECTDIR}/ADC_Init.o ADC_Init.c    -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD) 
+	@${RM} ${OBJECTDIR}/ADC.o.d 
+	@${RM} ${OBJECTDIR}/ADC.o 
+	@${FIXDEPS} "${OBJECTDIR}/ADC.o.d" $(SILENT) -rsi ${MP_CC_DIR}../  -c ${MP_CC}  $(MP_EXTRA_CC_PRE)  -g -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -MMD -MF "${OBJECTDIR}/ADC.o.d" -o ${OBJECTDIR}/ADC.o ADC.c    -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD) 
 	
 endif
 
@@ -132,7 +132,7 @@ endif
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 dist/${CND_CONF}/${IMAGE_TYPE}/ADC_Initialization.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
 	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
-	${MP_CC} $(MP_EXTRA_LD_PRE) -g -mdebugger -D__MPLAB_DEBUGGER_REAL_ICE=1 -mprocessor=$(MP_PROCESSOR_OPTION)  -o dist/${CND_CONF}/${IMAGE_TYPE}/ADC_Initialization.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}          -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)   -mreserve=data@0x0:0x27F   -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D=__DEBUG_D,--defsym=__MPLAB_DEBUGGER_REAL_ICE=1,--no-code-in-dinit,--no-dinit-in-serial-mem,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--memorysummary,dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml
+	${MP_CC} $(MP_EXTRA_LD_PRE) -g -mdebugger -D__MPLAB_DEBUGGER_SIMULATOR=1 -mprocessor=$(MP_PROCESSOR_OPTION)  -o dist/${CND_CONF}/${IMAGE_TYPE}/ADC_Initialization.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}          -DXPRJ_default=$(CND_CONF)  -legacy-libc  $(COMPARISON_BUILD)      -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D=__DEBUG_D,--defsym=__MPLAB_DEBUGGER_SIMULATOR=1,--no-code-in-dinit,--no-dinit-in-serial-mem,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--memorysummary,dist/${CND_CONF}/${IMAGE_TYPE}/memoryfile.xml
 	
 else
 dist/${CND_CONF}/${IMAGE_TYPE}/ADC_Initialization.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
